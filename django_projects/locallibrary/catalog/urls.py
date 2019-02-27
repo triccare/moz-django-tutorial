c6119f1b2e1b5e0b5afcd7c2e00c4d4d8c449e42
triccare <triccare@gmail.com>
2019-02-26 23:26:10 -0500

    Tutorial completed through part 6

--- django_projects/locallibrary/catalog/urls.py
+++ django_projects/locallibrary/catalog/urls.py
@@ -3,4 +3,8 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
+    path('author/<int:pk>', views.AuthorDetailView.as_view(), name='author-detail'),
+    path('authors/', views.AuthorListView.as_view(), name='authors'),
+    path('book/<int:pk>', views.BookDetailView.as_view(), name='book-detail'),
+    path('books/', views.BookListView.as_view(), name='books'),
 ]
--- django_projects/locallibrary/catalog/views.py
+++ django_projects/locallibrary/catalog/views.py
@@ -1,5 +1,6 @@
 """Views"""
 from django.shortcuts import render
+from django.views import generic
 
 from catalog.models import Book, Author, BookInstance, Genre
 
@@ -25,3 +26,21 @@ def index(request):
 
     # Render the HTML template index.html with the data in the context variable
     return render(request, 'index.html', context=context)
+
+
+class AuthorListView(generic.ListView):
+    model = Author
+    paginate_by = 10
+
+
+class AuthorDetailView(generic.DetailView):
+    model = Author
+
+
+class BookListView(generic.ListView):
+    model = Book
+    paginate_by = 10
+
+
+class BookDetailView(generic.DetailView):
+    model = Book
